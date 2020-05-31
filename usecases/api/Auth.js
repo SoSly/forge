a3869fc856da4542cfb9821e14d763df634b54c3
Kevin Kragenbrink <kevin@writh.net>
2020-05-30 23:53:46 -0700

    Redirect user to original resource after auth

--- usecases/api/Auth.js
+++ usecases/api/Auth.js
@@ -3,6 +3,8 @@ import Passport from 'koa-passport';
 import Router from '@koa/router';
 import User from '../../domain/User.js';
 
+const RETURN_TO_COOKIE = 'forge.returnTo';
+
 class Auth {
     constructor(config) {
         // configure passport user serialization
@@ -25,27 +27,37 @@ class Auth {
         const http = config.get('HTTP');
         const discord = config.get('Discord');
         const strategy = new DiscordStrategy({
-            callbackURL: `${http.protocol}://${http.host}:${http.port}/auth/discord/callback`,
+            callbackURL: `${http.protocol}://${http.host}/auth/discord/callback`,
             clientID: discord.client,
             clientSecret: discord.secret,
             failureRedirect: '/',
             scope: ['identify'],
-            successRedirect: '/',
         }, this.verifyStrategy);
         Passport.use(strategy);
 
         // configure routes
         this.router = new Router();
-        this.router.get('/auth/discord', Passport.authenticate('discord', {successReturnToOrRedirect: '/profile'}));
-        this.router.get('/auth/discord/callback', Passport.authenticate('discord'));
+        this.router.get('/auth/discord', this.handleAuth);
+        this.router.get('/auth/discord/callback', this.handleAuth, this.handleAuthCallback);
     }
 
     ensureAuth(ctx, next) {
-        console.log(ctx.isAuthenticated());
         if (!ctx.isAuthenticated()) {
+            const returnTo = ctx.cookies.get(RETURN_TO_COOKIE);
+            if (returnTo === undefined) ctx.cookies.set(RETURN_TO_COOKIE, ctx.path);    
             return ctx.redirect('/auth/discord');
         }
         return next();
+    }
+
+    handleAuth(ctx, next) {
+        return Passport.authenticate('discord')(ctx, next);
+    }
+
+    handleAuthCallback(ctx, next) {
+        const returnTo = ctx.cookies.get(RETURN_TO_COOKIE);
+        ctx.cookies.set(RETURN_TO_COOKIE, '');
+        ctx.redirect(returnTo);
     }
 
     async verifyStrategy(accessToken, refreshToken, profile, done) {
